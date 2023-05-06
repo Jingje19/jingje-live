c6ea1bbf729da8e9ff07596281299cbf1e3ab455
Jingje19 <94737052+Jingje19@users.noreply.github.com>
2023-05-06 14:01:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "jjdatabase.ciq41lwggd5j.us-east-1.rds.amazonaws.com"
+customuser = "jingje19"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jingjesql"
+customregion = "us-east-1"
+
