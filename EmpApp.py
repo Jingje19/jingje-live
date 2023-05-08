9039b3738c73a49515dcd6b99fa4aa1f91951911
Jingje19 <94737052+Jingje19@users.noreply.github.com>
2023-05-08 18:50:06 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -57,7 +57,7 @@ def AddEmp():
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id)
         emp_body = "emp id: "+ str(emp_id) + "\nfirst name: "+ str(first_name) + "\nlast name: "+ str(last_name) +"\nBirth Date: "+ str(birth_date) +"\Gender: "+ str(gender)
         +"\nEmail: "+ str(email) +"\nPhone: "+ str(phone) +"\nAddress: "+ str(address) +"\nDepartment: "+ str(department) +"\nJob Title: "+ str(job)
-        +"\Start Date: "+ str(date)
+        +"\nStart Date: "+ str(date)
         s3 = boto3.resource('s3')
 
         try:
