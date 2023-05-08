6fbcf9287806b09d15dffe0425043b8ce37bcb4b
Jingje19 <94737052+Jingje19@users.noreply.github.com>
2023-05-08 21:54:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.ciq41lwggd5j.us-east-1.rds.amazonaws.com"
 customuser = "jingje19"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "jingjesql"
+custombucket = "kwanjingje-bucket"
 customregion = "us-east-1"
 
