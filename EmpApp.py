a8f6e8d9b0c317cfbee2042e9fdd286a1312c3c8
Jingje19 <94737052+Jingje19@users.noreply.github.com>
2023-05-08 21:13:14 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -28,7 +28,7 @@ def home():
 
 @app.route("/aboutKJJ", methods=['GET','POST'])
 def about1():
-    return render_template('www.jingje.s3-website-us-east-1.amazonaws.com')
+    return redirect("http://jingje.s3-website-us-east-1.amazonaws.com")
 
 
 @app.route("/aboutHR", methods=['GET','POST'])
